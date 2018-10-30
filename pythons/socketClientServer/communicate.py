ec1c64a4dae97f0ff725d943441d6d0afc083efd
nanHome <nan@nanhome.com>
2018-10-30 22:32:51 +0800

    ...

--- pythons/socketClientServer/communicate.py
+++ pythons/socketClientServer/communicate.py
@@ -7,12 +7,12 @@ import logging
 import queue
 
 commConfigs={
-    "timeOut":10,
+    "timeOut":0.1,
     "MangerServeListCnt":3
 }
 
 class commServer(object):
-    def __init__(self, host='127.0.0.1', port=77777, timeout=2, client_nums=10):
+    def __init__(self, host='127.0.0.1', port=77777, timeout=1, client_nums=10):
         self.__host = host
         self.__port = port
         self.__timeout = timeout
@@ -20,7 +20,13 @@ class commServer(object):
         self.__buffer_size = 4096
 
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.setblocking(False)
+        # no matter what the operate, recv send accept ...
+        # blocking=false socket, will return rightnow, so will recv/send multi times to
+        # complete a true recv/send
+        # blocking=true socket, will wait for the recv/send data(unless the Tcp's half package, ),
+        # self.server.setblocking(False)
+        self.server.setblocking(True)
+
         self.server.settimeout(self.__timeout)
         self.server.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1) #keepalive
         self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #端口复用
